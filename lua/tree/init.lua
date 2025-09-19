b54cc74ee3b69b2652555280f4096592eab59bc6
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-09-18 22:03:22 -0400

    refactor: add cache, populate on setup, add refresh keymap

--- lua/tree/init.lua
+++ lua/tree/init.lua
@@ -3,6 +3,9 @@ local M = {}
 local ns_id = vim.api.nvim_create_namespace "Tree"
 
 local TREE_INSTANCE = nil
+local SETUP_CALLED = false
+local TREE_CACHE = {}
+local LOCKED = false
 
 --- @generic T
 --- @param val T | nil
@@ -50,46 +53,6 @@ end
 --- @field icon_char string
 --- @field icon_hl string
 
---- @class IndentLinesOpts
---- @field chunk string[]
---- @field cwd string
---- @param opts IndentLinesOpts
-local function indent_lines(opts)
-  --- @type IndentedLine[]
-  local lines = {}
-  for _, str in ipairs(opts.chunk) do
-    local period_pos = str:find "%."
-    if not period_pos then goto continue end
-
-    local prefix_length = period_pos - 1
-    local whitespace = string.rep(" ", prefix_length / 2)
-    local filename = str:sub(period_pos)
-
-    local rel_path = vim.fs.normalize(filename)
-    local abs_path = vim.fs.joinpath(opts.cwd, rel_path)
-
-    local type = (function()
-      local stat_res = vim.uv.fs_stat(abs_path)
-      if not stat_res then
-        return "file"
-      end
-      return stat_res.type
-    end)()
-
-    --- @type IndentedLine
-    local line = {
-      whitespace = whitespace,
-      abs_path = abs_path,
-      type = type,
-    }
-    table.insert(lines, line)
-
-
-    ::continue::
-  end
-  return lines
-end
-
 --- @param lines FormattedLine[]
 local function get_curr_buf_line(lines, curr_buf_abs_path)
   for idx, line in ipairs(lines) do
@@ -98,36 +61,100 @@ local function get_curr_buf_line(lines, curr_buf_abs_path)
   return nil
 end
 
---- @class FormatLinesOpts
---- @field lines IndentedLine[]
+--- @class GetFormattedLinesOpts
+--- @field str string
+--- @field cwd string
 --- @field icons_enabled boolean
---- @param opts FormatLinesOpts
-local function format_lines(opts)
-  local formatted_lines = {}
-  for _, indented_line in ipairs(opts.lines) do
-    local basename = vim.fs.basename(indented_line.abs_path)
-    local icon_info = get_icon_info { abs_path = indented_line.abs_path, icon_type = indented_line.type, icons_enabled = opts.icons_enabled, }
-    local formatted = ("%s%s %s"):format(indented_line.whitespace, icon_info.icon_char, basename)
+--- @param opts GetFormattedLinesOpts
+local function get_formatted_line(opts)
+  local period_pos = opts.str:find "%."
+  if not period_pos then return nil end
 
-    --- @type FormattedLine
-    local formatted_line = {
-      abs_path = indented_line.abs_path,
-      whitespace = indented_line.whitespace,
-      type = indented_line.type,
-      formatted = formatted,
-      icon_char = icon_info.icon_char,
-      icon_hl = icon_info.icon_hl,
-    }
-    table.insert(formatted_lines, formatted_line)
-  end
+  local prefix_length = period_pos - 1
+  local whitespace = string.rep(" ", prefix_length / 2)
+  local filename = opts.str:sub(period_pos)
 
-  return formatted_lines
+  local rel_path = vim.fs.normalize(filename)
+  local abs_path = vim.fs.joinpath(opts.cwd, rel_path)
+
+  local type = (function()
+    local stat_res = vim.uv.fs_stat(abs_path)
+    if not stat_res then
+      return "file"
+    end
+    return stat_res.type
+  end)()
+
+  local basename = vim.fs.basename(abs_path)
+  local icon_info = get_icon_info { abs_path = abs_path, icon_type = type, icons_enabled = opts.icons_enabled, }
+  local formatted = ("%s%s %s"):format(whitespace, icon_info.icon_char, basename)
+
+  --- @type FormattedLine
+  local formatted_line = {
+    whitespace = whitespace,
+    abs_path = abs_path,
+    type = type,
+    formatted = formatted,
+    icon_char = icon_info.icon_char,
+    icon_hl = icon_info.icon_hl,
+
+  }
+  return formatted_line
 end
 
 --- @param mark_name string
 local function is_buffer_mark_unset(mark_name)
   local mark = vim.api.nvim_buf_get_mark(0, mark_name)
   return mark[1] == 0 and mark[2] == 0
+end
+
+--- @class SetupOpts
+--- @field cwd string
+--- @field icons_enabled boolean
+--- @param opts SetupOpts
+local populate_tree_cache = function(opts)
+  if LOCKED then return vim.notify "[tree.nvim] locked" end
+  LOCKED = true
+  TREE_CACHE = {}
+  vim.notify "[tree.nvim] Populating the cache ..."
+  vim.system({ "tree", "-f", "-a", "--gitignore", "--noreport", "--charset=ascii", }, {
+    cwd = opts.cwd,
+  }, function(obj)
+    local process = coroutine.create(function()
+      for _, str in ipairs(vim.split(obj.stdout, "\n")) do
+        local formatted_line = get_formatted_line {
+          cwd = opts.cwd,
+          icons_enabled = opts.icons_enabled,
+          str = str,
+        }
+        if formatted_line == nil then goto continue end
+
+        table.insert(TREE_CACHE, formatted_line)
+        coroutine.yield()
+        ::continue::
+      end
+    end)
+
+    local function continue_processing()
+      coroutine.resume(process)
+
+      if coroutine.status(process) == "suspended" then
+        vim.schedule(continue_processing)
+      end
+    end
+    vim.schedule(continue_processing)
+    LOCKED = false
+    vim.schedule(function() vim.notify "[tree.nvim] Cache populated" end)
+  end)
+end
+
+--- @param opts SetupOpts
+M.setup = function(opts)
+  SETUP_CALLED = true
+  opts = default(opts, {})
+  opts.cwd = default(opts.cwd, vim.uv.cwd())
+  opts.icons_enabled = default(opts.icons_enabled, true)
+  populate_tree_cache(opts)
 end
 
 --- @class TreeOpts
@@ -137,10 +164,14 @@ end
 --- @field win_width number
 
 --- @class TreeKeymaps
---- @field [string] "close-tree"|"select-focus-win"|"select-focus-tree"|"select-close-tree"
+--- @field [string] "close-tree"|"select-focus-win"|"select-focus-tree"|"select-close-tree"|"refresh"
 
 --- @param opts TreeOpts
 M.tree = function(opts)
+  if not SETUP_CALLED then
+    error "[tree.nvim] `setup` must be called before `tree`"
+  end
+
   if TREE_INSTANCE and vim.api.nvim_win_is_valid(TREE_INSTANCE) then
     vim.api.nvim_set_current_win(TREE_INSTANCE)
     local is_mark_set = not is_buffer_mark_unset "a"
@@ -204,32 +235,93 @@ M.tree = function(opts)
   vim.api.nvim_set_option_value("cursorline", true, { win = tree_winnr, })
   vim.api.nvim_win_set_buf(tree_winnr, tree_bufnr)
 
-  --- @type FormattedLine[]
-  local lines = {}
-
   local close_tree = function()
+    if LOCKED then return vim.notify "[tree.nvim] locked" end
     vim.api.nvim_win_close(tree_winnr, true)
   end
 
   local select = function()
+    if LOCKED then return vim.notify "[tree.nvim] locked" end
+
     local line_nr = vim.fn.line "."
-    local line = lines[line_nr]
+    local line = TREE_CACHE[line_nr]
     if line.type ~= "file" then return end
 
     vim.api.nvim_set_current_win(curr_winnr)
     vim.cmd("edit " .. vim.trim(line.abs_path))
   end
 
+  local refresh = function()
+    if LOCKED then return vim.notify "[tree.nvim] locked" end
+
+    LOCKED = true
+    TREE_CACHE = {}
+    vim.api.nvim_buf_set_lines(tree_bufnr, 0, -1, false, {})
+    vim.notify "[tree.nvim] Refreshing ..."
+    vim.system({ "tree", "-f", "-a", "--gitignore", "--noreport", "--charset=ascii", }, {
+      cwd = cwd,
+      stdout = function(err, data)
+        if err then return end
+        if not data then return end
+        vim.schedule(function()
+          local formatted_lines = {}
+
+          local chunk = vim.split(data, "\n")
+          for idx, str in ipairs(chunk) do
+            local formatted_line = get_formatted_line { cwd = cwd, icons_enabled = opts.icons_enabled, str = str, }
+            if formatted_line == nil then goto continue end
+            table.insert(formatted_lines, formatted_line)
+
+            vim.api.nvim_buf_set_lines(tree_bufnr, #TREE_CACHE + idx - 1, -1, false, { formatted_line.formatted, })
+
+            local icon_hl_col_0_indexed = #formatted_line.whitespace
+            local row_1_indexed = #TREE_CACHE + idx
+            local row_0_indexed = row_1_indexed - 1
+
+            vim.hl.range(
+              tree_bufnr,
+              ns_id,
+              formatted_line.icon_hl,
+              { row_0_indexed, icon_hl_col_0_indexed, },
+              { row_0_indexed, icon_hl_col_0_indexed + 1, }
+            )
+            vim.cmd "redraw"
+
+            ::continue::
+          end
+
+          TREE_CACHE = vim.list_extend(TREE_CACHE, formatted_lines)
+        end)
+      end,
+    }, function()
+      vim.schedule(function()
+        local curr_bufnr_line = get_curr_buf_line(TREE_CACHE, bufname_abs_path)
+        if curr_bufnr_line then
+          vim.api.nvim_win_set_cursor(tree_winnr, { curr_bufnr_line, 0, })
+          vim.api.nvim_buf_set_mark(0, "a", curr_bufnr_line, 0, {})
+        end
+        vim.cmd "normal! zz"
+        LOCKED = false
+        vim.notify "[tree.nvim] Done refreshing"
+      end)
+    end)
+  end
+
   local keymap_fns = {
+    refresh = refresh,
     ["close-tree"] = close_tree,
     ["select-close-tree"] = function()
+      if LOCKED then return vim.notify "[tree.nvim] locked" end
+
       select()
       close_tree()
     end,
     ["select-focus-win"] = select,
     ["select-focus-tree"] = function()
+      if LOCKED then return vim.notify "[tree.nvim] locked" end
+
       local line_nr = vim.fn.line "."
-      local line = lines[line_nr]
+      local line = TREE_CACHE[line_nr]
       if line.type ~= "file" then return end
 
       vim.api.nvim_win_call(curr_winnr, function()
@@ -244,83 +336,33 @@ M.tree = function(opts)
     end, { buffer = tree_bufnr, })
   end
 
-  vim.system({ "tree", "-f", "-a", "--gitignore", "--noreport", "--charset=ascii", }, {
-    cwd = cwd,
-    stdout = function(err, data)
-      if err then return end
-      if not data then return end
-      vim.schedule(function()
-        local formatted_lines = {}
+  local process = coroutine.create(function()
+    for idx, formatted_line in ipairs(TREE_CACHE) do
+      vim.api.nvim_buf_set_lines(tree_bufnr, #TREE_CACHE + idx - 1, -1, false, { formatted_line.formatted, })
 
-        local chunk = vim.split(data, "\n")
-        for idx, str in ipairs(chunk) do
-          local period_pos = str:find "%."
-          if not period_pos then goto continue end
+      local icon_hl_col_0_indexed = #formatted_line.whitespace
+      local row_1_indexed = #TREE_CACHE + idx
+      local row_0_indexed = row_1_indexed - 1
 
-          local prefix_length = period_pos - 1
-          local whitespace = string.rep(" ", prefix_length / 2)
-          local filename = str:sub(period_pos)
-
-          local rel_path = vim.fs.normalize(filename)
-          local abs_path = vim.fs.joinpath(cwd, rel_path)
-
-          local type = (function()
-            local stat_res = vim.uv.fs_stat(abs_path)
-            if not stat_res then
-              return "file"
-            end
-            return stat_res.type
-          end)()
-
-          local basename = vim.fs.basename(abs_path)
-          local icon_info = get_icon_info { abs_path = abs_path, icon_type = type, icons_enabled = opts.icons_enabled, }
-          local formatted = ("%s%s %s"):format(whitespace, icon_info.icon_char, basename)
-
-          --- @type FormattedLine
-          local formatted_line = {
-            whitespace = whitespace,
-            abs_path = abs_path,
-            type = type,
-            formatted = formatted,
-            icon_char = icon_info.icon_char,
-            icon_hl = icon_info.icon_hl,
-
-          }
-          table.insert(formatted_lines, formatted_line)
-
-          vim.api.nvim_buf_set_lines(tree_bufnr, #lines + idx - 1, -1, false, { formatted, })
-
-          local icon_hl_col_0_indexed = #whitespace
-          local row_1_indexed = #lines + idx
-          local row_0_indexed = row_1_indexed - 1
-
-          vim.hl.range(
-            tree_bufnr,
-            ns_id,
-            icon_info.icon_hl,
-            { row_0_indexed, icon_hl_col_0_indexed, },
-            { row_0_indexed, icon_hl_col_0_indexed + 1, }
-          )
-          vim.cmd "redraw"
-
-          ::continue::
-        end
-
-        lines = vim.list_extend(lines, formatted_lines)
-      end)
-    end,
-  }, function()
-    vim.schedule(function()
-      -- vim.api.nvim_set_option_value("modifiable", false, { buf = tree_bufnr, })
-
-      local curr_bufnr_line = get_curr_buf_line(lines, bufname_abs_path)
-      if curr_bufnr_line then
-        vim.api.nvim_win_set_cursor(tree_winnr, { curr_bufnr_line, 0, })
-        vim.api.nvim_buf_set_mark(0, "a", curr_bufnr_line, 0, {})
-      end
-      vim.cmd "normal! zz"
-    end)
+      vim.hl.range(
+        tree_bufnr,
+        ns_id,
+        formatted_line.icon_hl,
+        { row_0_indexed, icon_hl_col_0_indexed, },
+        { row_0_indexed, icon_hl_col_0_indexed + 1, }
+      )
+      vim.cmd "redraw"
+      coroutine.yield()
+    end
   end)
+
+  local function continue_processing()
+    coroutine.resume(process)
+    if coroutine.status(process) == "suspended" then
+      vim.schedule(continue_processing)
+    end
+  end
+  vim.schedule(continue_processing)
 end
 
 -- M.tree {
